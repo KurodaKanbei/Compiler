6c02c993350e316b84282bc1e16094069de52143
KurodaKanbei <812395846@qq.com>
2018-05-28 19:01:29 +0800

    redirect output

--- src/Compiler/Main.java
+++ src/Compiler/Main.java
@@ -59,11 +59,11 @@ public class Main {
 
     private static void translate() throws IOException {
         String assembly = Translator.getAssembly();
-        System.err.println(assembly);
-        File file = new File("program.asm");
+        System.out.println(assembly);
+        /*File file = new File("program.asm");
         FileOutputStream fileOutputStream = new FileOutputStream(file);
         byte[] bytes = assembly.getBytes();
         fileOutputStream.write(bytes);
-        fileOutputStream.close();
+        fileOutputStream.close();*/
     }
 }
