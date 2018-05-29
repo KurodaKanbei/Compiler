17eeb37526901a94cfc154a17c71ee2cec1148bf
KurodaKanbei <812395846@qq.com>
2018-05-30 01:22:47 +0800

    疯狂安排

--- src/Compiler/Opt/Optimize.java
+++ src/Compiler/Opt/Optimize.java
@@ -5,7 +5,7 @@ import Compiler.CFG.ProgramIR;
 public class Optimize {
     public static void optimize() {
         ProgramIR.getFunctionMap().values().forEach(functionIR -> {
-            BinaryInstructionRazor.uselessBinaryInstructionRemove(functionIR);
+            //BinaryInstructionRazor.uselessBinaryInstructionRemove(functionIR);
             BinaryInstructionRazor.uselessMoveInstructionRemove(functionIR);
             LivenessAnalyst.analysis(functionIR);
             RegisterAllocator.naiveAllocate(LivenessAnalyst.getEdge(), LivenessAnalyst.getCount(), functionIR);
