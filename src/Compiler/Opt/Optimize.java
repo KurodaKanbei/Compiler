86d10c596430cd4c26c4793d10514d54366d6d26
KurodaKanbei <812395846@qq.com>
2018-05-30 01:23:56 +0800

    被安排了

--- src/Compiler/Opt/Optimize.java
+++ src/Compiler/Opt/Optimize.java
@@ -5,9 +5,9 @@ import Compiler.CFG.ProgramIR;
 public class Optimize {
     public static void optimize() {
         ProgramIR.getFunctionMap().values().forEach(functionIR -> {
+            LivenessAnalyst.analysis(functionIR);
             //BinaryInstructionRazor.uselessBinaryInstructionRemove(functionIR);
             BinaryInstructionRazor.uselessMoveInstructionRemove(functionIR);
-            LivenessAnalyst.analysis(functionIR);
             RegisterAllocator.naiveAllocate(LivenessAnalyst.getEdge(), LivenessAnalyst.getCount(), functionIR);
         });
     }
