0f86d42869a2e1c219933a7c59018d6a28528efd
KurodaKanbei <812395846@qq.com>
2018-06-03 00:09:41 +0800

    deleteHunter

--- src/Compiler/Opt/ImmediateHunter.java
+++ src/Compiler/Opt/ImmediateHunter.java
@@ -4,11 +4,9 @@ import Compiler.CFG.Block;
 import Compiler.CFG.FunctionIR;
 import Compiler.CFG.Instruction.BinaryInstruction;
 import Compiler.CFG.Instruction.Instruction;
-import Compiler.CFG.Instruction.MoveInstruction;
 import Compiler.CFG.Instruction.UnaryInstruction;
 import Compiler.CFG.Operand.ImmediateOperand;
 import Compiler.CFG.Operand.Operand;
-import Compiler.CFG.Operand.VirtualRegister;
 
 public class ImmediateHunter {
 
@@ -16,7 +14,7 @@ public class ImmediateHunter {
         for (Block block : functionIR.getBlockList()) {
             for (int i = 0; i < block.getInstructionList().size(); i++) {
                 Instruction instruction = block.getInstructionList().get(i);
-                if (instruction instanceof MoveInstruction) {
+                /*if (instruction instanceof MoveInstruction) {
                     Operand target = ((MoveInstruction) instruction).getTarget();
                     Operand source = ((MoveInstruction) instruction).getSource();
                     if (target instanceof VirtualRegister && source instanceof ImmediateOperand
@@ -24,7 +22,7 @@ public class ImmediateHunter {
                         block.getInstructionList().remove(i);
                         block.getInstructionList().add(i, new BinaryInstruction(BinaryInstruction.BinaryOp.XOR, target, target));
                     }
-                }
+                }*/
                 if (instruction instanceof BinaryInstruction) {
                     Operand target = ((BinaryInstruction) instruction).getTarget();
                     Operand source = ((BinaryInstruction) instruction).getSource();
