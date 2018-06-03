a505f2d8655657c2a0f4f30261c4a3437894f468
KurodaKanbei <812395846@qq.com>
2018-06-03 09:49:11 +0800

    fix razor

--- src/Compiler/Opt/Optimize.java
+++ src/Compiler/Opt/Optimize.java
@@ -38,5 +38,8 @@ public class Optimize {
             SuperBlockBuilder.buildSuperBlock(functionIR);
             SuperBlockBuilder.uselessJumpRemove(functionIR);
         }
+        for (FunctionIR functionIR : ProgramIR.getFunctionMap().values()) {
+            BinaryInstructionRazor.uselessReturnInstructionRemove(functionIR);
+        }
     }
 }
